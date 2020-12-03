a8379f297aee169542c827e86446b5b80f188dfe
Krishna Kant <lushkant@gmail.com>
2020-12-03 15:37:04 +0530

    Update files

--- functions.php
+++ functions.php
@@ -4,10 +4,17 @@
  *
  * Place any custom functionality/code snippets here.
  *
- * @since Ink Child 1.0
+ * @since Ink Child 1.0.1
  */
 
+/**
+ * Include child theme styles.
+ *
+ * @return void
+ */
 function ink_child_styles() {
-    wp_enqueue_style( 'ink-parent-theme-css', get_template_directory_uri() . '/style.css' );
+    $theme = wp_get_theme();
+	wp_enqueue_style( 'stag-style', get_template_directory_uri() . '/style.css', array(), $theme->parent()->get( 'Version' ) );
+	wp_enqueue_style( 'ink-child-style', get_stylesheet_uri(), array( 'stag-style' ), $theme->get( 'Version' ) );
 }
 add_action( 'wp_enqueue_scripts', 'ink_child_styles' );
